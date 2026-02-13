7c510b19ffc280b9d229a5428bdde767fb698234
alejogomez21 <alejogoji777@gmail.com>
2026-02-13 12:49:30 -0500

    Guardando mi trabajo de la semana 02

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,20 @@
-package application;
-
-import javax.print.CancelablePrintJob;
-import java.sql.SQLOutput;
-
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-
-
         String nombre = "Alejo";
         int edad = 20;
         boolean masculino = true;
-
         System.out.println("My name is " + nombre);
         System.out.println("Iam " + edad + " years old");
         System.out.println("Am i male? " + masculino);
-
         int fechaActual = 2026;
         int fechaNacimiento = 2006;
         int calculadoraEdad = fechaActual - fechaNacimiento;
         System.out.println("Mi edad es: " + calculadoraEdad);
 
-        for (int i = 1; i <= 10; i++){
+        for(int i = 1; i <= 10; ++i) {
             System.out.println("Interation: " + i);
         }
+
     }
 }
