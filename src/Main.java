0602c188f82e3017c53bf7ea93dfc8f937ab72e9
alejogomez21 <alejogoji777@gmail.com>
2026-02-14 13:56:34 -0500

    trabajo terminado de semana03

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,16 @@ import java.util.List;
 
 public class Main {
     final static int CURRENT_YEAR = 2026;
+    static List<Integer> ageStudentsList = new ArrayList<>();
+
     public static void main(String[] args) {
         final String GREETING_MESSAGE = "Saludo estudiantes de java";
 
         String[] namesStudents = {"Alejo" , "Juan" , "Isabela" , "Camilo" , "Laura"};
         int[] birthYears = {2000, 1998, 2002, 1995, 2001} ;
 
-        List<int[]> ageStudentsList = new ArrayList<>();
-
         showMessage(GREETING_MESSAGE);
+        listStudents(namesStudents, fillAgesList(birthYears));
 
     }
     static void showMessage(String message){
@@ -21,6 +22,21 @@ public class Main {
     static int calculateAge(int birthYear){
         return CURRENT_YEAR - birthYear;
     }
+    static  List<Integer> fillAgesList(int[] birthYears){
 
+        for (int birthYear : birthYears){
+            int age = calculateAge(birthYear);
+            ageStudentsList.add(age);
+        }
+        return ageStudentsList;
+    }
 
+    static void listStudents(String[] namesStudents, List<Integer> agesStudents) {
+        showMessage("Lista de estudiantes: ");
+        for (int i = 0; i < namesStudents.length; i++){
+            showMessage(namesStudents[i] + " - Edad: " + agesStudents.get(i));
+        }
+    }
 }
+
+
