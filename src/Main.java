19958d8bb33e6fcf5676045c1bcb6807fc0dba98
alejogomez21 <alejogoji777@gmail.com>
2026-02-14 12:04:33 -0500

    definicion de variables y metodos

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        String nombre = "Alejo";
-        int edad = 20;
-        boolean masculino = true;
-        System.out.println("My name is " + nombre);
-        System.out.println("Iam " + edad + " years old");
-        System.out.println("Am i male? " + masculino);
-        int fechaActual = 2026;
-        int fechaNacimiento = 2006;
-        int calculadoraEdad = fechaActual - fechaNacimiento;
-        System.out.println("Mi edad es: " + calculadoraEdad);
+import java.util.ArrayList;
+import java.util.List;
 
-        for(int i = 1; i <= 10; ++i) {
-            System.out.println("Interation: " + i);
-        }
+public class Main {
+    final static int CURRENT_YEAR = 2026;
+    public static void main(String[] args) {
+        final String GREETING_MESSAGE = "Saludo estudiantes de java";
+
+        String[] namesStudents = {"Alejo" , "Juan" , "Isabela" , "Camilo" , "Laura"};
+        int[] birthYears = {2000, 1998, 2002, 1995, 2001} ;
+
+        List<int[]> ageStudentsList = new ArrayList<>();
+
+        showMessage(GREETING_MESSAGE);
 
     }
+    static void showMessage(String message){
+        System.out.println(message);
+    }
+
+    static int calculateAge(int birthYear){
+        return CURRENT_YEAR - birthYear;
+    }
+
+
 }
